6c63ae0264496fa559c1e6ebb0171a85aaeabb99
Michael Scheffenacker <michael.scheffenacker@gmail.com>
2018-12-20 01:21:26 +0100

    fixed and improved invoice-template.php  - fixed gender selection  - improved output of address lines

--- invoice-template.php
+++ invoice-template.php
@@ -14,15 +14,25 @@ function eur_price($price) {
     return "€\,$f_price";
 }
 
+$company = $customer->company;
 $full_name = "$customer->title $customer->forename $customer->surname";
 $purpose = $invoice->reference;
+if ($company) {
+    $address_first_lines = "$company & $purpose \\\\\n";
+    $address_first_lines .= "z.\,H. $full_name &\\\\\n";
+}
+else {
+    $address_first_lines = "$full_name & $purpose \\\\\n";
+}
 $date = date("j.n.Y", strtotime($invoice->invoice_date));
 $vatin = ($customer->vatin != "") ? "UID: $customer->vatin" : "";
-if ($customer->gender = 'x') {
+$gender = $customer->gender;
+if ($gender == 'none') {
     $salutation = 'geehrte Damen und Herren';
 }
 else {
-    $salutation = ($customer->gender == 'm') ? 'geehrter Herr' : 'geehrte Frau ' . $customer->surname;
+    $salutation = ($gender == 'male') ? 'geehrter Herr ' : 'geehrte Frau ';
+    $salutation .= $customer->surname;
 }
 $invoice_items = '';
 $sum_net = 0;
@@ -65,7 +75,7 @@ $f_sum_gross = eur_price($sum_gross);
 \ohead[]{}
 \ifoot[]{\renewcommand\tabcolsep{0pt}
 \begin{tabularx}{\textwidth}{@{} lYlYlYlYl @{}}
-Michael Scheffenacker & | & Klosterstraße 1, 5450 Werfen & | &  +43\,650\,980\,89\,85\, & | & michael.scheffenacker@formiculare.org & | & Hypo Tirol Bank, BIC: HYPTAT22, IBAN: AT765700030053302332
+Michael Scheffenacker & | & Klosterstraße 1, 5450 Werfen & | &  +43\,650\,980\,89\,85\, & | & michael.scheffenacker@gmail.com & | & Hypo Tirol Bank, BIC: HYPTAT22, IBAN: AT765700030053302332
 \end{tabularx}}
 \cfoot[]{}
 \ofoot[]{}
@@ -82,13 +92,12 @@ Michael Scheffenacker & | & Klosterstraße 1, 5450 Werfen & | &  +43\,650\,980\,
 Klosterstraße 1 & Rechnung Nr. <?php echo $invoice->invoice_number ?> \\
 5450 Werfen & <?php echo $date ?> \\
  +43\,650\,980\,89\,85\,& UID: ATU67000639 \\
- michael.scheffenacker@formiculare.org \\
+ michael.scheffenacker@gmail.com \\
  \end{tabularx}
 
 \begin{tabularx}{\textwidth}{@{} p{10cm}l @{}}
 \textit{Rechnung an:} & \textit{Für:}\\
-<?php echo $customer->company ?>& <?php echo $purpose?>\\
-z.\,H.<?php echo $full_name ?> &\\
+<?php echo $address_first_lines ?>
 <?php echo $customer->street ?>  &\\
 <?php echo $customer->city ?> &\\
 <?php echo $customer->country ?> &\\
@@ -97,7 +106,8 @@ z.\,H.<?php echo $full_name ?> &\\
 
 
 \vspace*{0cm}Sehr <?php echo $salutation ?>,\\
-wie vereinbart erlaube ich mir, erbrachte Leistungen wie folgt in Rechnung zu stellen:
+wie vereinbart erlaube ich mir,
+erbrachte Leistungen wie folgt in Rechnung zu stellen:
 
 \vspace*{.6cm}
 
@@ -115,7 +125,8 @@ Summe Netto & & & <?php echo $f_sum_net ?> \\
 
  \vspace*{1cm}
  
-Hiermit stelle ich Ihnen den Betrag von <?php echo $f_sum_gross ?> in Rechnung. Zahlbar innerhalb von 14 Tagen.
+Hiermit stelle ich Ihnen den Betrag von <?php echo $f_sum_gross ?> in Rechnung.
+Zahlbar innerhalb von 14 Tagen.
 
 Kontoinhaber: Michael Scheffenacker\\
 Bankinstitut: Hypo Tirol Bank\\
