2c75ac498805b796be7987163b4fc947a3da9f78
Michael Scheffenacker <michael.scheffenacker@gmail.com>
2019-03-25 09:19:38 +0100

    refactoring StyleTests: changed assertEquals() > assertSame()

--- tests/StyleTests.php
+++ tests/StyleTests.php
@@ -11,6 +11,7 @@ use PHPUnit\Framework\TestCase;
 require_once __DIR__ . '/../includes/html/generic_html_generators.php';
 require_once __DIR__ . '/../includes/view/StyledFields.php';
 require_once __DIR__ . '/../includes/view/TextStyle.php';
+require_once __DIR__ . '/../includes/view/DropDownStyle.php';
 require_once __DIR__ . '/TestRecord.php';
 require_once __DIR__ . '/TestIdRecord.php';
 
@@ -20,7 +21,7 @@ class StyleTests extends TestCase {
             generate_text_input('one', 'one')
             . generate_text_input('two', 'two');
         $styled_record = new StyledFields(new TestRecord());
-        $this->assertEquals($expected_html, $styled_record->generate_html());
+        $this->assertSame($expected_html, $styled_record->generate_html());
     }
 
     public function test_readonly_form_html() {
@@ -29,7 +30,7 @@ class StyleTests extends TestCase {
             . generate_text_input('two', 'two');
         $styled_record = new StyledFields(new TestRecord());
         $styled_record->set_field_readonly('one');
-        $this->assertEquals($expected_html, $styled_record->generate_html());
+        $this->assertSame($expected_html, $styled_record->generate_html());
     }
 
     public function test_id_default_readonly_form_html() {
@@ -39,7 +40,7 @@ class StyleTests extends TestCase {
         $styled_record = new StyledFields(new TestIdRecord());
         // in contrary to test_readonly_form_html() no manual setting of
         // readonly is required.
-        $this->assertEquals($expected_html, $styled_record->generate_html());
+        $this->assertSame($expected_html, $styled_record->generate_html());
     }
 
     public function test_drop_down_style() {
